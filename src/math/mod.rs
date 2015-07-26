d3a305242aa7dddd90261b69f65657a83579acc2
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-07-26 18:16:31 -0500

    Changed ownership of math functions
    They now take ownership, that is `x` in `ln(x)` is moved. This should be
    the standard style whenever the memory can actually be re-used.

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -5,25 +5,26 @@ use tensor::Tensor;
 use Numeric;
 use num::traits::Float;
 
-macro_rules! add_impl0 {
+macro_rules! add_impl {
     ($($f:ident)*) => ($(
-        pub fn $f<T: Numeric + Float>(x: &Tensor<T>) -> Tensor<T> {
-            let mut y = Tensor::zeros(&x.shape());
-            for i in 0..x.size() {
-                y[i] = x[i].$f();
+        pub fn $f<T: Numeric + Float>(x: Tensor<T>) -> Tensor<T> {
+            //let mut y = Tensor::zeros(&x.shape());
+            let mut y = x;
+            for i in 0..y.size() {
+                y[i] = y[i].$f();
             }
             y
         }
     )*)
 }
 
-add_impl0! { ln log10 log2 sin cos tan asin acos atan exp_m1
-             ln_1p sinh cosh tanh asinh acosh atanh }
+add_impl! { ln log10 log2 sin cos tan asin acos atan exp_m1
+            ln_1p sinh cosh tanh asinh acosh atanh sqrt }
 
-pub fn log<T: Numeric + Float>(x: &Tensor<T>, base: T) -> Tensor<T> {
-    let mut y = Tensor::zeros(&x.shape());
-    for i in 0..x.size() {
-        y[i] = x[i].log(base);
+pub fn log<T: Numeric + Float>(x: Tensor<T>, base: T) -> Tensor<T> {
+    let mut y = x;
+    for i in 0..y.size() {
+        y[i] = y[i].log(base);
     }
     y
 }
