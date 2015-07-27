98eb251bd34e5c4cac20d00a7984a31a4b38d5d0
Gustav Larsson <gustav.m.larsson@gmail.com>
2015-07-26 19:41:18 -0500

    Moved function in file

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -22,14 +22,6 @@ add_impl! { ln log10 log2 sin cos tan asin acos atan exp_m1 exp exp2
             ln_1p sinh cosh tanh asinh acosh atanh sqrt
             floor ceil round trunc fract abs signum }
 
-pub fn log<T: Numeric + Float>(x: Tensor<T>, base: T) -> Tensor<T> {
-    let mut y = x;
-    for i in 0..y.size() {
-        y[i] = y[i].log(base);
-    }
-    y
-}
-
 macro_rules! add_impl_to_bool {
     ($($f:ident)*) => ($(
         pub fn $f<T: Numeric + Float>(x: &Tensor<T>) -> Tensor<bool> {
@@ -43,3 +35,12 @@ macro_rules! add_impl_to_bool {
 }
 
 add_impl_to_bool! { is_nan is_finite is_infinite is_normal is_sign_positive is_sign_negative }
+
+pub fn log<T: Numeric + Float>(x: Tensor<T>, base: T) -> Tensor<T> {
+    let mut y = x;
+    for i in 0..y.size() {
+        y[i] = y[i].log(base);
+    }
+    y
+}
+
