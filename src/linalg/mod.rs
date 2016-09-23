5556cb73dd4649a89ae8fa5895aafa2025f547de
Gustav Larsson <gustav.m.larsson@gmail.com>
2016-09-22 21:27:06 -0700

    Add linalg::diag function

--- src/linalg/mod.rs
+++ src/linalg/mod.rs
@@ -14,5 +14,29 @@
 //! let x = a.solve(&b);
 //! ```
 
+use tensor::Tensor;
+use std::cmp::min;
+use num::traits::Zero;
+
 mod solve;
 mod svd;
+
+/// If passed a vector, creates a diagonal matrix with the vector as its diagonal.
+/// If passed a matrix, the diagonal is extracted and returned.
+pub fn diag<T: Copy + Zero>(a: &Tensor<T>) -> Tensor<T> {
+    assert!(a.ndim() == 1 || a.ndim() == 2, "Can only run diag for vectors and matrices");
+    if a.ndim() == 1 {
+        let mut b = Tensor::zeros(&[a.size(), a.size()]);
+        for i in 0..a.size() {
+            b[(i, i)] = a[(i,)];
+        }
+        b
+    } else {
+        let mn = min(a.dim(0), a.dim(1));
+        let mut b = Tensor::zeros(&[mn]);
+        for i in 0..mn {
+            b[(i,)] = a[(i, i)];
+        }
+        b
+    }
+}
